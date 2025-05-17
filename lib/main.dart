9fa8cfd92d529a869a1ab90cab451d69a114bbb8
erikmiqueias <erikmiqueiaspereira@outlook.com>
2025-05-17 18:43:23 -0400

    feat: lock screen orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:todo_list_sqlite/data/app_colors.dart';
 import 'package:todo_list_sqlite/screens/login.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF181F34),
-          brightness: Brightness.dark,
-        ),
-        scaffoldBackgroundColor: AppColors.backgroundPrimary,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.backgroundPrimary,
-          foregroundColor: Colors.white,
-        ),
-        fontFamily: 'Poppins',
-        outlinedButtonTheme: OutlinedButtonThemeData(
-          style: OutlinedButton.styleFrom(
-            textStyle: TextStyle(fontFamily: 'Poppins'),
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
+    (_) => runApp(
+      MaterialApp(
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFF181F34),
+            brightness: Brightness.dark,
+          ),
+          scaffoldBackgroundColor: AppColors.backgroundPrimary,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: AppColors.backgroundPrimary,
+            foregroundColor: Colors.white,
+          ),
+          fontFamily: 'Poppins',
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: OutlinedButton.styleFrom(
+              textStyle: TextStyle(fontFamily: 'Poppins'),
+            ),
           ),
         ),
+        debugShowCheckedModeBanner: false,
+        home: const LoginScreen(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
     ),
   );
 }
