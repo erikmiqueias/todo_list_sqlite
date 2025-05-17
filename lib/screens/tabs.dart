8a2298dcc07da8da0aa125d1e65523af6c2aaa8b
erikmiqueias <erikmiqueiaspereira@outlook.com>
2025-05-17 15:04:22 -0400

    refactor: rename Todos screen to Tasks

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -52,7 +52,7 @@ class _TabsScreenState extends State<TabsScreen> {
         onTap: _selectPage,
         currentIndex: _activePageIndex,
         items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Todos'),
+          BottomNavigationBarItem(icon: Icon(Icons.list), label: 'Tasks'),
           BottomNavigationBarItem(
             icon: Icon(Icons.calendar_month),
             label: 'Calendar',
